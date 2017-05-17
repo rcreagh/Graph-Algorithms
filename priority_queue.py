7ff073a24f198d4dd632cc30d22d5b660a7db685
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-05-17 22:50:59 +0100

    Update priority queue with add task function

--- priority_queue.py
+++ priority_queue.py
@@ -2,12 +2,24 @@
 """This script is to create a class Priority Queue from scratch without the use
 of Python's heapq library. """
 
+import itertools
+
 class PriorityQueue:
     """Priority queue implementation."""
 
-    def __init__(self):
+    def __init__(self, tasks_prios = None):
         self.pq = []
         self.entry_finder = {}
+        self.counter = itertools.count()
+        if tasks_prios:
+            for task, priority in tasks_prios:
+                self.add_task(task, priority)
 
     def add_task(self, task, priority=0):
-        pass
+        if task in self.entry_finder:
+            del self.entry_finder[task]
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        #TODO: Add heapify here
+
