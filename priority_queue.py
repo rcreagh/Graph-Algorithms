fc13bcc7a686d4b1da42b2a225fbbc249be62676
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-05-17 22:31:51 +0100

    Beginning of priority queue.

---
+++ priority_queue.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+"""This script is to create a class Priority Queue from scratch without the use
+of Python's heapq library. """
+
+class PriorityQueue:
+    """Priority queue implementation."""
+
+    def __init__(self):
+        self.pq = []
+        self.entry_finder = {}
+
+    def add_task(self, task, priority=0):
+        pass
