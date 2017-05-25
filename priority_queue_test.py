54530897b8eac1f807014ab12133c58dfd17d534
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-05-25 19:24:49 +0100

    Add unit tests to priority queue.

--- priority_queue_test.py
+++ priority_queue_test.py
@@ -13,7 +13,60 @@ class PriorityQueueTest(unittest.TestCase):
             [0, 3],
             [1, 5],
             [2, 1],
-            [1, 2]])
+            [0, 2]])
 
         self.assertRaises(priority_queue.PriorityQueueDuplicateTaskException,
-                priority_queue.PriorityQueue(queue))
+                priority_queue.PriorityQueue, queue)
+
+    def testPriorityQueue_PopEmptyQueue(self):
+        queue = priority_queue.PriorityQueue()
+        self.assertRaises(priority_queue.EmptyPriorityQueueException, 
+                priority_queue.PriorityQueue.pop, queue)
+
+    def testPriorityQueue_FromTuple(self):
+        queue = priority_queue.PriorityQueue([
+            [0, 5],
+            [1, 3],
+            [2, 8],
+            [3, 6],
+            [4, 2],
+            [5, 7],
+            [6, 1],
+            [7, 5]])
+
+        expected = [
+            [6, 1],
+            [1, 3],
+            [4, 2],
+            [7, 5],
+            [0, 5],
+            [2, 8],
+            [5, 7],
+            [3, 6]]
+
+        self.assertEqual(expected, queue.pq)
+
+    def testPriorityQueue_Pop(self):
+        queue = priority_queue.PriorityQueue([
+            [0, 5],
+            [1, 3],
+            [2, 8],
+            [3, 6],
+            [4, 2],
+            [5, 7],
+            [6, 1],
+            [7, 5]])
+
+        expected_task = [6, 1]
+        expected_remaining_queue = [
+            [4, 2],
+            [1, 3],
+            [3, 6],
+            [7, 5],
+            [0, 5],
+            [2, 8],
+            [5, 7]]
+
+        task = queue.pop()
+        self.assertEqual(expected_task, task)
+        self.assertEqual(expected_remaining_queue, queue.pq)
