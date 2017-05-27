e014da2bc06e4d455a8d3b3ca7e457c5d503e7f6
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-05-27 11:37:11 +0100

    Start breadth first search algorithm.

---
+++ breadth_first_search.py
@@ -0,0 +1,6 @@
+#! usr/bin/python
+"""This script uses breadth first search to find the minimal spanning tree of a
+graph. This is a special case used only for unweighted graphs."""
+
+def breadth_first_search(graph):
+    pass
