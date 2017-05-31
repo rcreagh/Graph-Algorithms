8f2efbaf31e95722b8c4c63e9791f5fe0ec00307
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-05-31 22:34:24 +0100

    Add unit test for UnionFind,find()

--- union_find_test.py
+++ union_find_test.py
@@ -8,13 +8,23 @@ import unittest
 
 class UntionFindTest(unittest.TestCase):
 
-    def testFind(node):
-        component_1 = {}
-        component_1[0] = 1
-        component_1[1] = 2
-        component_1[2] = 5
-        component_1[3] = 4
-        component_1[4] = 5
+    def testFind(self):
+        ufs = union_find.UnionFind()
+        ufs.roots = {}
+        ufs.roots[0] = 1
+        ufs.roots[1] = 2
+        ufs.roots[2] = 5
+        ufs.roots[3] = 4
+        ufs.roots[4] = 4
+        ufs.roots[5] = 5
+        
+        parents = []
+        for i in range(0, 6):
+            parents.append(ufs.find(i))
 
+        expected = [5, 5, 5, 4, 4, 5]
+
+        self.assertEqual(expected, parents)
+        
 if __name__ == '__main__':
     unittest.main()
