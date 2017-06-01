8e155b7939c25e0fedd032e19a84554d1e3fd3b5
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-06-01 19:42:29 +0100

    Update unit test for union_find

--- union_find_test.py
+++ union_find_test.py
@@ -25,6 +25,17 @@ class UntionFindTest(unittest.TestCase):
         expected = [5, 5, 5, 4, 4, 5]
 
         self.assertEqual(expected, parents)
-        
+
+    def testUnion(self):
+        ufs = union_find.UnionFind()
+        ufs.union(0, 1) # Point 0 to root 1.
+        ufs.union(0, 2) # Point 2 to root of 0 (1).
+        ufs.union(3, 4) # Point 3 to root 4.
+        ufs.union(4, 5) # Point 5 to root of 4 (4).
+        ufs.union(2, 3) # Point path from 2 to root of 2 (1) to root of 3 (4).
+
+        # TODO: Add expected and assert equals
+        pass
+
 if __name__ == '__main__':
     unittest.main()
