5f8a6775a26d00bfaa7e274166008fdce7b53386
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2017-06-05 14:52:53 +0100

    Add unit tests for kruskal.

--- kruskal.py
+++ kruskal.py
@@ -1,40 +1,51 @@
 #! usr/bin/python
 """This script is a Python implementation of Kruskal's greedy algorithm to find
-the minimum spanning tree of a weighted, connected graph. This is not an efficient way to find the minimum spanning tree of a graph and is for learning
-purposes only. This implementation is for use with Python's Networkx graph
-library.
+the minimum spanning tree of a weighted, connected graph. If the graph is
+unweighted, it considers all edges to have the same weight. 
+
+This is not an efficient way to find the minimum spanning tree of a graph and
+is for learning purposes only. This implementation is for use with Python's
+Networkx graph library.
 
 NOTE: This is WIP."""
 
 import networkx
 import union_find
 
-def kruskal(graph):
+class KruskalDisconnectedGraphException(Exception):
+    """An exception raised if disconnected graph is passed."""
+    pass
+
+def kruskal(graph, return_networkx_graph=False):
     """Find the minimal spanning tree of a weighted, connected graph.
     
     Args:
         graph: Weighted graph in edge list representation.
+        return_networkx_graph: Set output to be a networkx graph object.
     Returns:
-        List of weighted edges making up the minimal spanning tree of the graph.
+        Either a list of weighted edges making up the minimal spanning tree of
+        the graph, or a networkx graph of these weighted edges.
     """
-    #TODO: Raise exception if data type is not networkx graph.
-    #TODO: Add exception if graph is not connected.
     if not networkx.is_connected(graph):
-        raise pass
+        raise KruskalDisconnectedGraphException(
+                "Graph passed in is not connected.")
     minimal_spanning_tree = []
     edges = graph.edges(data=True)
-    #TODO: Add exception if unweighted graph passed in.
     try:
         sorted_edges = sorted(edges, key=lambda x: x[2])
     except:
-        pass
+        sorted_edges = edges
     total_nodes = graph.size()
     current_tree_size = 0
     ufs = union_find.UnionFind()
     for i, j, w in sorted_edges:
         if ufs.find(i) != ufs.find(j):
-            minimal_spanning_tree.append([(i, j, w)])
+            minimal_spanning_tree.append((i, j, w))
             ufs.union(i, j)
             current_tree_size += 1
+            # When minimal_spanning_tree has grown to n-1 edges, stop.
             if current_tree_size == total_nodes - 1:
-                return minimal_spanning_tree
+                if return_networkx_graph:
+                    return networkx.Graph(minimal_spanning_tree)
+                else:
+                    return minimal_spanning_tree
--- kruskal_test.py
+++ kruskal_test.py
@@ -2,10 +2,58 @@
 """Unit tests for kruskal.py.
 
 NOTE: This is WIP."""
+import kruskal
+import networkx
+import unittest
 
-G.add_edge('a','b',weight=0.6)
-G.add_edge('a','c',weight=0.2)
-G.add_edge('c','d',weight=0.1)
-G.add_edge('c','e',weight=0.7)
-G.add_edge('c','f',weight=0.9)
-G.add_edge('a','d',weight=0.3)
+class KruskalTest(unittest.TestCase):
+    
+    def testKruskalDisconnectedGraph(self):
+        graph = networkx.Graph()
+        graph.add_edge(0, 1, weight=0.6)
+        graph.add_edge(1, 2, weight=0.2)
+        graph.add_edge(1, 3, weight=0.1)
+        graph.add_edge(2, 4, weight=0.7)
+        graph.add_edge(5, 6, weight=0.9)
+        graph.add_edge(0, 4, weight=0.3)
+
+        self.assertRaises(kruskal.KruskalDisconnectedGraphException,
+                kruskal.kruskal, graph)
+
+    def testKruskalWeightedGraph(self):
+        graph = networkx.Graph()
+        graph.add_edge(0, 1, weight=0.6)
+        graph.add_edge(1, 2, weight=0.2)
+        graph.add_edge(1, 3, weight=0.1)
+        graph.add_edge(2, 4, weight=0.7)
+        graph.add_edge(2, 5, weight=0.9)
+        graph.add_edge(2, 6, weight=0.3)
+        graph.add_edge(6, 3, weight=1.1)
+
+        expected = [
+                (1, 3, {'weight': 0.1}), (1, 2, {'weight': 0.2}),
+                (2, 6, {'weight': 0.3}), (0, 1, {'weight': 0.6}),
+                (2, 4, {'weight': 0.7}), (2, 5, {'weight': 0.9})]
+
+        self.assertEqual(expected, kruskal.kruskal(graph))
+
+    def testKruskalWeightedGraphReturnNxGraph(self):
+        graph = networkx.Graph()
+        graph.add_edge(0, 1, weight=0.6)
+        graph.add_edge(1, 2, weight=0.2)
+        graph.add_edge(1, 3, weight=0.1)
+        graph.add_edge(2, 4, weight=0.7)
+        graph.add_edge(2, 5, weight=0.9)
+        graph.add_edge(2, 6, weight=0.3)
+        graph.add_edge(6, 3, weight=1.1)
+
+        expected = [
+                (1, 3, {'weight': 0.1}), (1, 2, {'weight': 0.2}),
+                (2, 6, {'weight': 0.3}), (0, 1, {'weight': 0.6}),
+                (2, 4, {'weight': 0.7}), (2, 5, {'weight': 0.9})]
+
+        self.assertItemsEqual(expected, kruskal.kruskal(graph,
+            return_networkx_graph=True).edges(data=True))
+
+if __name__ == '__main__':
+    unittest.main()
